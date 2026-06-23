1bf8eceb19e68145aea5c2f47c538df450200ce4
Jeppe Rosendahl Steen <jeppe-steen@live.dk>
2026-06-23 11:49:44 +0200

    creating new/better project

---
+++ app/layouts/default.vue
@@ -0,0 +1,41 @@
+<script setup>
+    const navItems = [
+        {label: 'home', to: '/', type: 'link' },
+        {label: 'log ind', to: '/login', type: 'link', styling: 'secondary' }
+    ]
+</script>
+
+<template>
+    <UiNavigation :navItems="navItems">
+        <template #icon>
+            <h2>Logo her!</h2>
+        </template>
+    </UiNavigation>
+
+    <main class="content-container">
+        <div class="content-container--content">
+            <slot></slot>
+        </div>
+    </main>
+
+    <UiFooter />
+</template>
+
+<style scoped lang="scss">
+    .content-container {
+        width: 100%;
+        min-height: 100vh;
+        background-color: var(--background-color);
+
+        display: grid;
+        grid-template-columns: 1fr minmax(0, 70%) 1fr;
+        gap: 1rem;
+        grid-template-areas: '. content .';
+
+        &--content {
+            grid-area: content;
+            padding: 1rem;
+        }
+
+    }
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<script setup>
+useHead({
+    layout: 'default',
+})
+</script>
+
+<template>
+
+</template>
