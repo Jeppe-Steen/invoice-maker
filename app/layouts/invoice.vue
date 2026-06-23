173c473625930003d276694703e34f2b852cb25b
Jeppe Rosendahl Steen <jeppe-steen@live.dk>
2026-06-23 19:16:23 +0200

    layout invoice

---
+++ app/layouts/invoice.vue
@@ -0,0 +1,25 @@
+<template>
+    <main class="content-container">
+        <div class="content-container--content">
+            <slot></slot>
+        </div>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+    .content-container {
+        width: 100%;
+        min-height: 100vh;
+        background-color: var(--background-color);
+
+        display: grid;
+        grid-template-columns: 100%;
+        gap: 1rem;
+        grid-template-areas: 'content';
+
+        &--content {
+            grid-area: content;
+        }
+
+    }
+</style>
