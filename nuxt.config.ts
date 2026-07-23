2ce31d35f3410d842d235567b2a987e5ef455931
Jeppe Rosendahl Steen <jeppe-steen@live.dk>
2026-07-23 21:43:16 +0200

    update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,8 +4,8 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@jrs/core', '@nuxtjs/supabase'],
   supabase: {
-    url: import.meta.env.NUXT_PUBLIC_SUPABASE_URL,
-    key: import.meta.env.NUXT_PUBLIC_SUPABASE_KEY,
+    url: import.meta.env.NUXT_PUBLIC_SUPABASE_URL || process.env.NUXT_PUBLIC_SUPABASE_URL,
+    key: import.meta.env.NUXT_PUBLIC_SUPABASE_KEY || process.env.NUXT_PUBLIC_SUPABASE_KEY,
     redirect: false,
   },
   css: [
